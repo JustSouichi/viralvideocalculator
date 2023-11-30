60d665f851467244aff0502ebd84f6bb9a1ab279
TommasoBertocchiDeveloper <tommasobertocchideveloper04@gmail.com>
2023-11-30 19:46:38 +0100

    *

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+
+  return(
+    <>
+  <div className="flex flex-col items-center">
+    <p className="text-center text-2xl mt-10">
+      TikTok Viral Video Calculator
+    </p>
+    <div class="bg-white rounded-lg shadow-lg p-6 text-center text-2xl mt-10">
+      <h1 class="text-xl font-semibold mb-4">Your Box Title</h1>
+      <p class="text-gray-600">Box content goes here.</p>
     </div>
-  );
+  </div>
+    </>
+  )
 }
-
-export default App;
